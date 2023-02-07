49afcfba020143090946723f7d32d3861e0ec26c
MartinsRoberto <robertomartinsramm@gmail.com>
2023-02-07 00:51:33 -0300

    get, getAll, delete

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,8 +1,9 @@
+const Game = require('../models/Game')
 const GameModel = require('../models/Game')
 
 const gameController = {
   create: async (req, res) => {
-    try{
+    try {
       const game = {
         name: req.body.name,
         description: req.body.description,
@@ -12,12 +13,69 @@ const gameController = {
         devices: req.body.image
       }
 
+      const searchName = await GameModel.findOne({ name: game.name })
+
+      if (searchName) {
+        res.status(409).json({ msg: "Este jogo já foi criado" })
+        return
+      }
+
       const response = await GameModel.create(game)
 
-      console.log(response)
       res.status(201).json({ response, msg: "Jogo adicionado com sucesso" });
     }
-    catch(err){
+    catch (err) {
+      console.log(err)
+    }
+  },
+
+  getAll: async (req, res) => {
+    try {
+      const response = await GameModel.find()
+
+      res.status(200).json({ response })
+
+    }
+    catch (err) {
+      console.log(err)
+    }
+  },
+
+  get: async (req, res) => {
+    try {
+      const id = req.params.id
+
+      const game = await GameModel.findById(id)
+
+      if (!game) {
+        res.status(404).json({ msg: "Jogo não encontrada" });
+        return;
+      }
+
+      res.status(200).json({ game })
+
+    }
+    catch (err) {
+      console.log(err)
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id
+
+      const game = await GameModel.findById(id)
+
+      if (!game) {
+        res.status(404).json({ msg: "Jogo não encontrado" })
+      }
+
+      const deletedGame = await GameModel.findByIdAndDelete(id)
+
+      res.status(200).json({ deletedGame, msg: "Jogo deletado com sucesso" })
+
+    }
+    catch (err) {
       console.log(err)
     }
   }
--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -3,6 +3,11 @@ const router = require('express').Router()
 const gameController = require('../controllers/gameController')
 
 router.route('/game').post((req, res) => gameController.create(req, res))
-// router.post('/car/create', gameController.create)
+
+router.route('/game').get((req, res) => gameController.getAll(req, res))
+
+router.route('/game/:id').get((req, res) => gameController.get(req, res))
+
+router.route('/game/:id').delete((req, res) => gameController.delete(req, res))
 
 module.exports = router
